fc2cbed7bf565a8bfaefed7470e1a88f69b539e6
LZTK7 <2044290782@qq.com>
2026-04-15 23:05:25 +0800

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -2,7 +2,7 @@
 from calculator import add, subtract
 
 def test_add():
-    assert add(2, 3) == 5  # 断言2+3=5
+    assert add(2, 3) == 9999  # 断言2+3=5
 
 def test_subtract():
     assert subtract(5, 2) == 3  # 断言5-2=3
